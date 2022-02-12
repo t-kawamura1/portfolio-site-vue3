086ee2e8ed81cbc3f837576c39d9b27dc85b9f10
tkawamura <tkawamura.stayhome@gmail.com>
2022-02-12 10:42:49 +0900

    PROFILEのレイアウト修正

--- src/components/home-view/ProfileItem.vue
+++ src/components/home-view/ProfileItem.vue
@@ -1,62 +1,54 @@
 <template>
   <div class="profile-container">
-    <div class="profile-image-item">
-      <img src="@/assets/my-image-2.jpg" alt="作成者のイメージ" class="my-image">
-    </div>
-    <div class="profile-item">
-      <div class="my-name">河村 智之</div>
-      <p class="profile-text">
-        2022年現在、とある営業SaaSの自社開発企業に勤務。
-        京都府出身。
-        趣味はJリーグ観戦、ガンバ大阪サポーター。
-        2021年3月、思うところたくさんあって、5年間の公務員生活にピリオドを打つ。
-        世の中をもっと効率的に、かつおもしろいものにするために、エンジニアの道へ。
-      </p>
+    <div class="profile-wrapper">
+      <div class="profile-image-item">
+        <img src="@/assets/my-image-2.jpg" alt="作成者のイメージ" class="my-image">
+      </div>
+      <div class="profile-item">
+        <div class="my-name">河村 智之</div>
+        <p>2022年現在、とある営業SaaSの自社開発企業に勤務。</p>
+        <p>京都府出身、大阪在住</p>
+        <p>趣味はJリーグ観戦。ガンバ大阪サポーター。</p>
+        <p>2021年3月、自らの変革を求め、5年間の公務員生活にピリオドを打つ。</p>
+        <p>世の中をもっと効率的に、かつおもしろいものにするために、エンジニアの道へ。</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  setup(_, context) {
-    const onScroll = (name) => {
-      context.emit('scroll', name)
-    }
-
-    return {
-      onScroll
-    }
-  }
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
 .profile-container {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  .profile-image-item {
-    width: 40%;
-    flex-grow: 1;
-    position: relative;
-    .my-image {
-      width: 55%;
-      border-radius: 50%;
+  padding: 0 60px;
+  margin-bottom: 90px;
+  .profile-wrapper {
+    background-color: rgba(255, 255, 255, 0.5);
+    padding: 45px;
+    font-weight: 300;
+    display: flex;
+    justify-content: center;
+    .profile-image-item {
+      margin-right: 60px;
+      .my-image {
+        width: 250px;
+        border-radius: 50%;
+      }
     }
-  }
-  .profile-item {
-    width: 60%;
-    text-align: left;
-    vertical-align: middle;
-    flex-grow: 1;
-    margin-bottom: 30px;
-    .my-name {
-      margin-bottom: 30px;
-      font-size: 2rem;
-    }
-    .profile-text {
-      line-height: 2;
-      font-size: 1.5rem;
+    .profile-item {
+      text-align: left;
+      padding-top: 30px;
+      .my-name {
+        margin-bottom: 30px;
+        font-size: 2rem;
+      }
+      > p {
+        // margin-bottom: 20px;
+        line-height: 2.2;
+        font-size: 1.5rem;
+      }
     }
   }
 }
