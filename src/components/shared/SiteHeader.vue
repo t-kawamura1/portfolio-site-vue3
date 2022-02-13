b4615fa6dea03c5aac3c2c5980e70f02122262c4
tkawamura <tkawamura.stayhome@gmail.com>
2022-02-13 13:37:15 +0900

    モバイルの場合のヘッダーのjナビゲーションをハンバーガーメニューに変更

--- src/components/shared/SiteHeader.vue
+++ src/components/shared/SiteHeader.vue
@@ -1,26 +1,43 @@
 <template>
   <header class="header" :class="{ opacity: isOpacity }">
     <site-logo class="site-logo"/>
-    <nav class="home-nav" v-if="isHome">
-      <ul class="nav-list">
-        <li class="nav-list-item"
-            v-for="(name, index) in homeHeaderNameList"
-            :key="index"
-            @click="onScroll(name)">
-          {{ name }}
-        </li>
-      </ul>
-    </nav>
-    <nav class="pfdetail-nav" v-if="isPfDetail">
-      <ul class="nav-list">
-        <li class="nav-list-item"
-            v-for="(name, index) in pfDetailHeaderNameList"
-            :key="index"
-            @click="onScroll(name)">
-          {{ name }}
-        </li>
-      </ul>
-    </nav>
+    <div v-if="isMobile">
+      <nav class="hamberger-menu" @click="toggleDropdown()">
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul class="dropdown-list">
+          <li class="dropdown-list-item"
+              v-for="(name, index) in homeHeaderNameList"
+              :key="index"
+              @click="onScroll(name)">
+            {{ name }}
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div v-else>
+      <nav class="home-nav" v-if="isHome">
+        <ul class="nav-list">
+          <li class="nav-list-item"
+              v-for="(name, index) in homeHeaderNameList"
+              :key="index"
+              @click="onScroll(name)">
+            {{ name }}
+          </li>
+        </ul>
+      </nav>
+      <nav class="pfdetail-nav" v-if="isPfDetail">
+        <ul class="nav-list">
+          <li class="nav-list-item"
+              v-for="(name, index) in pfDetailHeaderNameList"
+              :key="index"
+              @click="onScroll(name)">
+            {{ name }}
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
@@ -34,19 +51,30 @@ export default {
     SiteLogo
   },
   setup(_, context) {
-    let isOpacity = ref(false)
-    let isHome = ref(false)
-    let isPfDetail = ref(false)
+    const isOpacity = ref(false)
+    const isMobile = ref(window.matchMedia('(max-width: 768px)').matches)
+    const isActive = ref(false)
+    const isHome = ref(false)
+    const isPfDetail = ref(false)
     const homeHeaderNameList = reactive([
       'PORTFOLIO',
       'SKILLS',
       'STANCES',
       'PROFILE'
-    ]);
+    ])
     const pfDetailHeaderNameList = reactive([
       'Lemon Sour DB',
       'T.KAWAMURA',
-    ]);
+    ])
+    const toggleDropdown = () => {
+      const hambergerMenu = document.querySelector('.hamberger-menu')
+      isActive.value = !(isActive.value)
+      if (isActive.value) {
+        hambergerMenu.classList.add('active')
+      } else {
+        hambergerMenu.classList.remove('active')
+      }
+    }
     const onScroll = (name) => {
       context.emit('scroll', name)
     }
@@ -70,11 +98,13 @@ export default {
     document.addEventListener.call(window, 'scroll', addOpacity)
 
     return {
+      isMobile,
       isOpacity,
       isHome,
       isPfDetail,
       homeHeaderNameList,
       pfDetailHeaderNameList,
+      toggleDropdown,
       onScroll,
       addOpacity,
     }
@@ -140,10 +170,65 @@ export default {
 @media (max-width: 768px) {
   .header {
     padding: 0 10px 0 10px;
-    justify-content: flex-start;
     height: 40px;
-    .site-logo {
-      margin-right: 30px;
+    .hamberger-menu {
+      width: 40px;
+      height: 40px;
+      position: relative;
+      cursor: pointer;
+      > span {
+        display: inline-block;
+        position: absolute;
+        left: 7px;
+        width: 26px;
+        height: 1.5px;
+        background-color: #636363;
+        border-radius: 1px;
+        transition: all 0.4s;
+        &:nth-of-type(1) {
+          top: 10px;
+        }
+        &:nth-of-type(2) {
+          top: 19px;
+        }
+        &:nth-of-type(3) {
+          top: 28px;
+        }
+      }
+
+      .dropdown-list {
+        padding: 0 10px;
+        position: absolute;
+        top: 100%;
+        right: 0;
+        background-color: rgba(255, 255, 255, 0.8);
+        opacity: 0;
+        transition: all 0.4s;
+        .dropdown-list-item {
+          padding: 12px 0;
+          cursor: pointer;
+          font-size: 1.5rem;
+        }
+      }
+
+      &.active {
+        > span {
+          &:nth-of-type(1) {
+            top: 19px;
+            transform: rotate(-45deg);
+          }
+          &:nth-of-type(2) {
+            opacity: 0;
+          }
+          &:nth-of-type(3) {
+            top: 19px;
+            transform: rotate(45deg);
+          }
+        }
+        .dropdown-list {
+          opacity: 1;
+        }
+      }
     }
     .home-nav {
       width: 270px;
